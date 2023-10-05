24b5ba2ce0a867ef9d91c534ae4615efde367f3f
RMehroj <bekmirzayevmehroj@gmail.com>
2023-10-05 16:10:34 +0500

    add Docker

--- onlineshop/celery.py
+++ onlineshop/celery.py
@@ -2,8 +2,8 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineshop.settings")
 
-app = Celery("core")
+app = Celery("onlineshop")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
